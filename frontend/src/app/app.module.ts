bda680d1d85b31a034c69d9954489c4643e35518
Catlin <clatcovschi@gmail.com>
2023-06-17 02:17:53 +0300

    app improvements

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AuthGuard} from "./client/auth/auth-guard.service";
+import {AuthService} from "./client/auth/auth.service";
+import {HttpClientModule} from "@angular/common/http";
+import {RoleGuard} from "./client/auth/role-guard.service";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService, RoleGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
