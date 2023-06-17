616a4ed52b75fa73bd7e01edf2aad7e7990a3dc7
Catlin <clatcovschi@gmail.com>
2023-06-17 06:27:29 +0300

    create search engine

---
+++ frontend/src/app/client/student/jobs/jobs-list/jobs-list.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {JobsService} from "../../../../server/jobs/jobs.service";
+import {Jobs} from "../../../../server/jobs/jobs";
+
+@Component({
+  selector: 'app-jobs-list',
+  templateUrl: './jobs-list.component.html',
+  styleUrls: ['./jobs-list.component.scss']
+})
+export class JobsListComponent implements OnInit {
+
+  constructor(private jobsService: JobsService) {
+  }
+
+  jobs: Jobs[] = [];
+
+  filteredData = this.jobs;
+  searchValue = '';
+
+  ngOnInit(): void {
+    this.jobsService.get().subscribe((res) => {
+      this.jobs = res;
+    })
+  }
+
+  search() {
+    this.filteredData = this.jobs.filter(item => {
+      return Object.values(item).some(value => {
+        if (typeof value === 'string') {
+          return value.toLowerCase().includes(this.searchValue.toLowerCase());
+        }
+        return false;
+      });
+    });
+  }
+
+}
--- frontend/src/app/client/student/student.module.ts
+++ frontend/src/app/client/student/student.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing.module';
 import { StudentComponent } from './student.component';
 import { HeaderComponent } from './header/header.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { JobsListComponent } from './jobs/jobs-list/jobs-list.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     StudentComponent,
-    HeaderComponent
+    HeaderComponent,
+    JobsComponent,
+    JobsListComponent
   ],
-  imports: [
-    CommonModule,
-    StudentRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        StudentRoutingModule,
+        FormsModule
+    ]
 })
 export class StudentModule { }
